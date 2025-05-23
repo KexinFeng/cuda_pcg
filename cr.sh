a86963c9238a29812729d07ae8af719fcd506763
kexin <fengx463@alumni.umn.edu>
2025-05-23 07:42:11 -0500

    cr.sh

--- cr.sh
+++ cr.sh
@@ -2,8 +2,9 @@ mkdir -p ~/mount_folder/cuda_pcg/qed_fermion_module
 cd ~/mount_folder/cuda_pcg/
 MAX_JOBS=${1:-2} python setup.py build_ext --inplace || { echo "Build failed. Exiting."; exit 1; }
 sleep 2  # Wait for 2 seconds to ensure build completion
+cp ~/mount_folder/cuda_pcg/qed_fermion_module/*.so ~/hmc/qed_fermion/qed_fermion/
+
 python unit_test/test_precon_vec.py
 python unit_test/test_mhm_vec.py
 python unit_test/test_b_vec_mul_per_tau.py
 # compute-sanitizer --tool=memcheck python unit_test/test_mhm_vec.py
-cp ~/mount_folder/cuda_pcg/qed_fermion_module/*.so ~/hmc/qed_fermion/qed_fermion/
